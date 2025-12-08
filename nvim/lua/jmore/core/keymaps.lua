96d2d55fa07c78ae9459711406b02af12815f3b1
Jon <jnthn18@gmail.com>
2025-12-08 12:54:51 -0700

    redo mason config

--- nvim/lua/jmore/core/keymaps.lua
+++ nvim/lua/jmore/core/keymaps.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "File [E]xplorer" })
 vim.keymap.set({ "n", "v", "x" }, "<leader>v", "<Cmd>edit $MYVIMRC<CR>", { desc = "Edit " .. vim.fn.expand("$MYVIMRC") })
 vim.keymap.set({ "n", "v", "x" }, "<leader>o", "<Cmd>source %<CR>", { desc = "Source " .. vim.fn.expand("$MYVIMRC") })
 vim.keymap.set({ "n", "v", "x" }, "<leader>O", "<Cmd>restart<CR>", { desc = "Restart vim." })
-vim.keymap.set({ "n", "v", "x" }, "<leader>lf", vim.lsp.buf.format, { desc = "Format current buffer" })
+vim.keymap.set({ "n", "v", "x" }, "<leader>f", vim.lsp.buf.format, { desc = "Format current buffer" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
