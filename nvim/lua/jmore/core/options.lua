552f7f74b7b891a2c5fe6cb0a60341e9a55df3cb
Jon <jnthn18@gmail.com>
2025-12-08 10:52:53 -0700

    add gitsigns

--- nvim/lua/jmore/core/options.lua
+++ nvim/lua/jmore/core/options.lua
@@ -1,27 +1,27 @@
 -- Set netrw to tree style
 vim.cmd("let g:netrw_liststyle = 3")
 
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.undofile = true
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.undofile = true
 
-vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-vim.opt.shiftwidth = 2 -- 2 spaces for indent width
-vim.opt.expandtab = true -- expand tab to spaces
-vim.opt.autoindent = true -- copy indent from current line when starting new one
-vim.opt.wrap = false
+vim.o.tabstop = 2 -- 2 spaces for tabs (prettier default)
+vim.o.shiftwidth = 2 -- 2 spaces for indent width
+vim.o.expandtab = true -- expand tab to spaces
+vim.o.autoindent = true -- copy indent from current line when starting new one
+vim.o.wrap = false
 
-vim.opt.ignorecase = true -- ignore case when searching
-vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-insensitive
+vim.o.ignorecase = true -- ignore case when searching
+vim.o.smartcase = true -- if you include mixed case in your search, assumes you want case-insensitive
 
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn="yes"
-vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+vim.o.cursorline = true
+vim.o.termguicolors = true
+vim.o.signcolumn="yes"
+vim.o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+vim.o.clipboard = "unnamedplus" -- use system clipboard as default register
 
-vim.opt.splitright = true -- split vertical window to the right
-vim.opt.splitbelow = true -- split horizontal window to the bottom
+vim.o.splitright = true -- split vertical window to the right
+vim.o.splitbelow = true -- split horizontal window to the bottom
 
-vim.opt.swapfile = false
-vim.opt.winborder = "rounded"
+vim.o.swapfile = false
+vim.o.winborder = "rounded"
