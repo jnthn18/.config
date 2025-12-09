7e8cdcf27ae2a5af7282ef3c87d49adfa35b7d2b
Jonathan More <jnthn18@gmail.com>
2025-12-09 13:37:26 -0700

    set nerd font

--- nvim/lua/jmore/core/options.lua
+++ nvim/lua/jmore/core/options.lua
@@ -1,6 +1,7 @@
 -- Set netrw to tree style
 vim.cmd("let g:netrw_liststyle = 3")
 
+vim.g.have_nerd_font = true
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.undofile = true
@@ -10,13 +11,15 @@ vim.o.shiftwidth = 2 -- 2 spaces for indent width
 vim.o.expandtab = true -- expand tab to spaces
 vim.o.autoindent = true -- copy indent from current line when starting new one
 vim.o.wrap = false
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.o.ignorecase = true -- ignore case when searching
 vim.o.smartcase = true -- if you include mixed case in your search, assumes you want case-insensitive
 
 vim.o.cursorline = true
 vim.o.termguicolors = true
-vim.o.signcolumn="yes"
+vim.o.signcolumn = "yes"
 vim.o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 vim.o.clipboard = "unnamedplus" -- use system clipboard as default register
 
